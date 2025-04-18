50a316876102b73ac81f8b70d7e2a15e99e58dfe
Ana Júlia <aanajulia505@gmail.com>
2025-04-17 22:42:08 -0300

    feat: comment validation added

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,15 +25,20 @@ export function Post({ author, publishedAt, content}) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment(event) {
+  function handleCreateNewComment() {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText('');
   }
   
-  function handleNewCommentChange(event) {
+  function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(){
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete){
@@ -42,6 +47,8 @@ export function Post({ author, publishedAt, content}) {
     })
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length == 0;
   
   return (
     <article className={styles.post}>
@@ -77,14 +84,17 @@ export function Post({ author, publishedAt, content}) {
 
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
+
         <textarea 
-        name='comment'
-        value={newCommentText}
-        placeholder="Deixe um comentário"
-        onChange={handleNewCommentChange}
+          name='comment'
+          value={newCommentText}
+          placeholder="Deixe um comentário"
+          onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>   
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>   
         </footer>
       </form>
 
